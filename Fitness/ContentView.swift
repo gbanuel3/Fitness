4706c748139853ee4229740b2113a4b72ef8e9c7
Gil Banuelos <gbanuel3@gmail.com>
2021-08-09 20:09:13 -0500

    added files

---
+++ Fitness/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Fitness
+//
+//  Created by Gil Banuelos on 8/9/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding().font(.title)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            ContentView()
+        }
+    }
+}
---
+++ Fitness/FitnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessApp.swift
+//  Fitness
+//
+//  Created by Gil Banuelos on 8/9/21.
+//
+
+import SwiftUI
+
+@main
+struct FitnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
